5ad55a3d828e2d7446392c6a3598314f56686ef2
asiajcrew <asia_jcrew96@yahoo.com>
2021-11-17 20:09:53 -0500

    added variables and console logs to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! Javascript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+console.log("This logs a screen");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+// fight();
